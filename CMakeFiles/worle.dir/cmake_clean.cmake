169c3f123c0d297487f3beaffaf8ca634f7b0045
fonsirada <fonsirada13@gmail.com>
2024-08-27 12:07:21 -0400

    added cmake files

---
+++ CMakeFiles/worle.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/worle.dir/main.cpp.o"
+  "CMakeFiles/worle.dir/main.cpp.o.d"
+  "CMakeFiles/worle.dir/wordle.cpp.o"
+  "CMakeFiles/worle.dir/wordle.cpp.o.d"
+  "worle"
+  "worle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/worle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
